b607247391a61667762ad0a87546f507bea364b3
Kaspar Birk <kaspar.birk@gmail.com>
2018-10-20 01:06:39 +0200

    Readded helpers

---
+++ helpers/classes/attachment.js
@@ -0,0 +1,148 @@
+'use strict';
+
+/**
+ * Dependencies
+ */
+const toCamelCase = require('../helpers/to-camel-case');
+const toSnakeCase = require('../helpers/to-snake-case');
+const deepClone = require('../helpers/deep-clone');
+
+/**
+ * Attachment class
+ */
+class Attachment {
+
+	/**
+	 * Constructor
+	 */
+  constructor(data) {
+
+    //Create from data
+    if (data) {
+      this.fromData(data);
+    }
+  }
+
+  /**
+   * From data
+   */
+  fromData(data) {
+
+    //Expecting object
+    if (typeof data !== 'object') {
+      throw new Error('Expecting object for Mail data');
+    }
+
+    //Convert to camel case to make it workable, making a copy to prevent
+    //changes to the original objects
+    data = deepClone(data);
+    data = toCamelCase(data);
+
+    //Extract properties from data
+    const {content, filename, type, disposition, contentId} = data;
+
+    //Set data
+    this.setContent(content);
+    this.setFilename(filename);
+    this.setType(type);
+    this.setDisposition(disposition);
+    this.setContentId(contentId);
+  }
+
+  /**
+   * Set content
+   */
+  setContent(content) {
+    //Duck type check toString on content if it's a Buffer as that's the method that will be called.
+    if (typeof content === 'string') {      
+      this.content = content;
+    } else if (content instanceof Buffer && content.toString !== undefined) {      
+      this.content = content.toString();
+    } else {
+      throw new Error('`content` expected to be either Buffer or string');
+    }
+
+    return;    
+  }
+
+  /**
+   * Set filename
+   */
+  setFilename(filename) {
+    if (typeof filename === 'undefined') {
+      return;
+    }
+    if (filename && typeof filename !== 'string') {
+      throw new Error('String expected for `filename`');
+    }
+    this.filename = filename;
+  }
+
+  /**
+   * Set type
+   */
+  setType(type) {
+    if (typeof type === 'undefined') {
+      return;
+    }
+    if (typeof type !== 'string') {
+      throw new Error('String expected for `type`');
+    }
+    this.type = type;
+  }
+
+  /**
+   * Set disposition
+   */
+  setDisposition(disposition) {
+    if (typeof disposition === 'undefined') {
+      return;
+    }
+    if (typeof disposition !== 'string') {
+      throw new Error('String expected for `disposition`');
+    }
+    this.disposition = disposition;
+  }
+
+  /**
+   * Set content ID
+   */
+  setContentId(contentId) {
+    if (typeof contentId === 'undefined') {
+      return;
+    }
+    if (typeof contentId !== 'string') {
+      throw new Error('String expected for `contentId`');
+    }
+    this.contentId = contentId;
+  }
+
+	/**
+	 * To JSON
+	 */
+  toJSON() {
+
+    //Extract properties from self
+    const {content, filename, type, disposition, contentId} = this;
+
+    //Initialize with mandatory properties
+    const json = {content, filename};
+
+    //Add whatever else we have
+    if (typeof type !== 'undefined') {
+      json.type = type;
+    }
+    if (typeof disposition !== 'undefined') {
+      json.disposition = disposition;
+    }
+    if (typeof contentId !== 'undefined') {
+      json.contentId = contentId;
+    }
+
+    //Return
+    return toSnakeCase(json);
+  }
+}
+
+//Export class
+module.exports = Attachment;
